04cf2fd36e159b8de6195280c09c2d2f12fe538a
sainathphad <phadsainath1@gmail.com>
2021-05-29 22:25:42 +0530

    ADDED: Mood Analyser Project Files

---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+    @Test
+    public void testMoodAnalysis() throws Exception {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+    }
+}
